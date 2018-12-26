f16d4070f503c18ebc6419da51456b50757932b7
Maxwell Patek <max7patek@gmail.com>
2018-12-26 18:06:11 -0500

    added gcal

---
+++ booker/gcal.py
@@ -0,0 +1,91 @@
+import os
+from datetime import timedelta
+import datetime
+import pytz
+from collections import namedtuple
+
+import httplib2
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+#global
+service_account_email = 'office-hour-booker@algorithmsbooker.iam.gserviceaccount.com'
+CLIENT_SECRET_FILE = 'secrets/AlgorithmsBooker-f554b310351f.json'
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+scopes = [SCOPES]
+default_cal = {
+    'kind': 'calendar#calendar',
+    'etag': '"2IG_UfzQLHbZ3_DsNnNPPvIpYxU/03vWfKLFalaz0L9Gm7obQolUlho"',
+    'id': 'nibko385n9mv06rudr4u0qk9ng@group.calendar.google.com',
+    'summary': 'Algorithms',
+    'timeZone': 'America/New_York',
+    'conferenceProperties': {
+        'allowedConferenceSolutionTypes': ['eventHangout']
+    }
+}
+
+def _build_service():
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        filename=CLIENT_SECRET_FILE,
+        scopes=SCOPES
+    )
+    http = credentials.authorize(httplib2.Http())
+    service = build('calendar', 'v3', http=http)
+    return service
+
+_service = _build_service()
+
+def get_service():
+    # check if service still valid
+    return _service
+
+
+def events_within(starttime, endtime=None):
+    if endtime is None:
+        endtime = starttime + datetime.timedelta(hours=24)
+    service = get_service()
+    page_token = None
+    while True:
+        resp = service.events().list(
+            calendarId=default_cal['id'],
+            pageToken=page_token,
+            timeMin=starttime.isoformat(),
+            timeMax=endtime.isoformat(),
+        ).execute()
+        yield from resp['items']
+        page_token = resp.get('nextPageToken')
+        if not page_token:
+            break
+
+def grant_write_permission(email):
+    service = get_service()
+    rule = {
+        'scope': {
+            'type': 'user',
+            'value': email,
+        },
+        'role': 'writer',
+    }
+    return service.acl().insert(calendarId=default_cal['id'], body=rule).execute()['id']
+
+
+def remove_write_permission(email):
+    service = get_service()
+    for rule in all_rule():
+        if rule['scope']['type'] == 'user' and rule['scope']['value'] == email:
+            service.acl().delete(calendarId=default_cal['id'], ruleId=rule['id'])
+
+
+def all_rules():
+    service = get_service()
+    page_token = None
+    while True:
+        resp = service.acl().list(
+            calendarId=default_cal['id'],
+            pageToken=page_token,
+        ).execute()
+        yield from resp['items']
+        page_token = resp.get('nextPageToken')
+        if not page_token:
+            break
--- booker/models.py
+++ booker/models.py
@@ -1,16 +1,42 @@
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from config import MINUTES_PER_BOOKING
 import datetime
 
+from booker.gcal import grant_write_permission, remove_write_permission
+
+class TA(models.Model):
+    email = models.CharField(max_length=50)
+
+@receiver(post_save, sender=TA)
+def add_permission(sender, **kwargs):
+    instance = kwargs['instance']
+    if kwargs['created']:
+        grant_write_permission(instance.email)
+
+@receiver(pre_delete, sender=TA)
+def remove_permission(sender, **kwargs):
+    instance = kwargs['instance']
+    remove_write_permission(instance.email)
+
+
+
 class OfficeHour(models.Model):
     starttime = models.DateTimeField()
     endtime = models.DateTimeField()
-    location = models.CharField(max_length=20)
+    location = models.CharField(max_length=20, default='contact staff')
+    event_id = models.CharField(max_length=50, unique=True, default='no event')
 
-    def from_event():
-        pass
+    @classmethod
+    def make_from_event(cls, event):
+        self = cls()
+        self.starttime = time.strptime(event['start']['datetime'], '%Y-%m-%dT%H:%M:%S.%f')
+        self.endtime = time.strptime(event['end']['datetime'], '%Y-%m-%dT%H:%M:%S.%f')
+        self.location = event['location']
+        self.event_id = event['id']
+        self.save()
+        return self
 
 
 class Bookable(models.Model):
