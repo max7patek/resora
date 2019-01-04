ff409263b1164e5727184f5b5b8360b6a344a1a3
Maxwell Patek <max7patek@gmail.com>
2019-01-04 01:35:05 -0500

    Split main into landing, bookables, and booking.

--- booker/urls.py
+++ booker/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
 from . import views
+from . import ajax
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', views.landing, name='landing'),
+    path('bookables', views.bookables, name='bookables'),
+    path('booking', views.booking, name='booking'),
     path('ta', views.as_ta, name="as-ta"),
-    path('ajax/all-bookables', views.all_bookables, name='all-bookables'),
-    path('ajax/book', views.book, name='book'),
-    path('ajax/release-booking', views.release_booking, name='release-booking'),
-    path('ajax/bookings', views.bookings, name='bookings'),
+    path('ajax/all-bookables', ajax.all_bookables, name='all-bookables'),
+    path('ajax/book', ajax.book, name='book'),
+    path('ajax/release-booking', ajax.release_booking, name='release-booking'),
+    path('ajax/bookings', ajax.bookings, name='bookings'),
 ]
