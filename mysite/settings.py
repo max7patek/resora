3d719269e9f6a0c252b187a6fb2188e635ed18c5
Maxwell Patek <max7patek@gmail.com>
2019-01-13 11:48:03 -0500

    push for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = KEYS.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '35.184.25.16', 'resora-ohs.com', 'www.obadiah.cs.virginia.edu', 'obadiah.cs.virginia.edu']
+ALLOWED_HOSTS = ['127.0.0.1', 'resora-ohs.com']
 
 
 # Application definition
